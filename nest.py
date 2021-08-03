b3305ad3eed32e59fd962e3079d9eee51088be24
Aaron Carnefix <25955012+acarnefix@users.noreply.github.com>
2021-08-03 00:12:35 -0500

    Use set temperature instead of ambient temperature

--- nest.py
+++ nest.py
@@ -8,23 +8,17 @@ import requests
 load_dotenv()
 
 CLIENT_ID = os.getenv("CLIENT_ID")
-print(CLIENT_ID)
-REDIRECT_URI = "https://www.aaron.com"
-CODE = "4/0AX4XfWj3qiFHzF4QoOjKizfRuT0zcvZWNtWEyoChcZT3bDgwDgFcM-zQoJwQuxSwBj0mZQ"
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+PROJECT_ID = os.getenv("PROJECT_ID")
+DEVICE_NAME = os.getenv("DEVICE_NAME")
+AUTH_URL = "https://www.googleapis.com/oauth2/v4/token"
+DEVICE_API_URL = "https://smartdevicemanagement.googleapis.com/v1"
+DEVICE_URL = f"{DEVICE_API_URL}/{DEVICE_NAME}"
 BASE_PARAMS = {
     "client_id": CLIENT_ID,
     "client_secret": "eVxaBhS0mN_lKVOtPVJI_ma9",
 }
-AUTH_URL = "https://www.googleapis.com/oauth2/v4/token"
-DEVICE_URL = "https://smartdevicemanagement.googleapis.com/v1"
-REFRESH_TOKEN = (
-    "1//0froojBrGVKx4CgYIARAAGA8SNwF-L9IrIjG9Q_iqN7brnFHjPi5SI84cKJdMg7uHe3QKn8J4yox41WQ7iPY4rK2IQuTdvsX8cAw"
-)
-PROJECT_ID = "acfdd0be-f091-4d4c-9e06-994512e485cb"
-DEVICE_NAME = (
-    "enterprises/acfdd0be-f091-4d4c-9e06-994512e485cb/devices/AVPHwEsBciJEQX1SGnvFUA5xAJbKOabGm7LhKt7ZsbmWfmYoUMWH7RCvC"
-    "4na1RalhEq96fIB7uxnPFFDKNlmhYQ9GQQ0cw"
-)
+
 
 
 def print_login_url():
@@ -38,7 +32,7 @@ def print_login_url():
 def get_tokens():
     token_params = dict(
         {
-            "code": CODE,
+            "code": os.getenv("CODE"),
             "grant_type": "authorization_code",
             "redirect_uri": REDIRECT_URI,
         },
@@ -53,7 +47,7 @@ def get_tokens():
 def refresh_access_token():
     refresh_params = dict(
         {
-            "refresh_token": REFRESH_TOKEN,
+            "refresh_token": os.getenv("REFRESH_TOKEN"),
             "grant_type": "refresh_token",
         },
         **BASE_PARAMS,
@@ -67,19 +61,23 @@ def request_headers():
 
 
 def get_device_name():
-    url_get_devices = f"{DEVICE_URL}/enterprises/{PROJECT_ID}/devices"
+    url_get_devices = f"{DEVICE_API_URL}/enterprises/{PROJECT_ID}/devices"
     response_json = requests.get(url_get_devices, headers=request_headers()).json()
     return response_json["devices"][0]["name"]
 
 
 def get_current_temperature():
-    url_get_device = f"{DEVICE_URL}/{DEVICE_NAME}"
-    response_json = requests.get(url_get_device, headers=request_headers()).json()
+    response_json = requests.get(DEVICE_URL, headers=request_headers()).json()
     return response_json["traits"]["sdm.devices.traits.Temperature"]["ambientTemperatureCelsius"]
 
 
+def get_current_set_temperature():
+    response_json = requests.get(DEVICE_URL, headers=request_headers()).json()
+    return response_json["traits"]["sdm.devices.traits.ThermostatTemperatureSetpoint"]["coolCelsius"]
+
+
 def execute_command(command_name, params):
-    url_set_mode = f"{DEVICE_URL}/{DEVICE_NAME}:executeCommand"
+    url_set_mode = f"{DEVICE_URL}:executeCommand"
     data = json.dumps({"command": f"sdm.devices.commands.{command_name}", "params": params})
     response = requests.post(url_set_mode, headers=request_headers(), data=data)
     print(response)
@@ -88,13 +86,9 @@ def execute_command(command_name, params):
 def set_new_temperature(increment):
     execute_command(
         "ThermostatTemperatureSetpoint.SetCool",
-        params={"coolCelsius": get_current_temperature() + increment}
+        params={"coolCelsius": get_current_set_temperature() + increment}
     )
 
 
 def set_fan_time(minutes):
     execute_command("Fan.SetTimer", params={"timerMode": "ON", "duration": f"{minutes*60}s"})
-
-
-# set_new_temperature(-1)
-set_fan_time(5)
